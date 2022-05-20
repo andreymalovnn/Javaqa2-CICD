42eeaa2c37558dd44dca20cc1081b809c5790f03
Andrey Malov <maa.pers@gmail.com>
2022-05-20 22:46:55 +0200

    Main filled with S.out for max index

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -2,6 +2,9 @@ package ru.netology.statistic;
 
 public class Main {
     public static void main(String[] args) {
-
+        long [] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        StatisticsService maxIncome = new StatisticsService();
+        long max = maxIncome.findMax(incomes);
+        System.out.println("Index of max income: " + max);
     }
 }
