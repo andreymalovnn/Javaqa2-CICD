e04b4049bb6c83c5f45feee9f3c8e716e52ff292
Andrey Malov <maa.pers@gmail.com>
2022-05-21 17:52:53 +0200

    Test added for 100% code coverage

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -2,9 +2,9 @@ package ru.netology.statistic;
 
 public class Main {
     public static void main(String[] args) {
-        long [] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long [] incomes = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         StatisticsService maxIncome = new StatisticsService();
         long max = maxIncome.findMax(incomes);
-        System.out.println("Index of max income: " + max);
+        System.out.println("Max income: " + max);
     }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,12 +9,18 @@ public class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void findMaxIndex() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
